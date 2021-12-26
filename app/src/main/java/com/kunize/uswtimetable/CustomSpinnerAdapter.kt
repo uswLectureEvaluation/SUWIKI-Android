9539f60ad443878ee097412c8a3a3a4df637d4d3
koownij <koownij@kakao.com>
2021-12-26 19:55:50 +0900

    customSpinner 추가 및 강의평가 레이아웃 일부 구현, 키보드 생성 시 바텀 네비게이션이 올라오는 현상 수정

---
+++ app/src/main/java/com/kunize/uswtimetable/CustomSpinnerAdapter.kt
@@ -0,0 +1,51 @@
+package com.kunize.uswtimetable
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.kunize.uswtimetable.R
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
+class CustomSpinnerAdapter(
+    var mContext: Context,
+    var spinnerNames: List<String>,
+    var spinnerImages: List<Int>
+) : ArrayAdapter<String?>(
+    mContext, R.layout.item_spinner_evaluation
+) {
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return getView(position, convertView, parent)
+    }
+
+    override fun getCount(): Int {
+        return spinnerNames.size
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var convertView = convertView
+        var mViewHolder = ViewHolder()
+        if (convertView == null) {
+            val mInflater =
+                mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = mInflater.inflate(R.layout.item_spinner_evaluation, parent, false)
+            mViewHolder.mImage =
+                convertView.findViewById<View>(R.id.imageview_spinner_image) as ImageView
+            mViewHolder.mName =
+                convertView.findViewById<View>(R.id.textview_spinner_name) as TextView
+            convertView.tag = mViewHolder
+        } else {
+            mViewHolder = convertView.tag as ViewHolder
+        }
+        mViewHolder.mImage!!.setImageResource(spinnerImages[position])
+        mViewHolder.mName!!.text = spinnerNames[position]
+        return convertView!!
+    }
+
+    private class ViewHolder {
+        var mImage: ImageView? = null
+        var mName: TextView? = null
+    }
+}
