672756f64f6cedf808a8381be322d389723e8ed8
koownij <koownij@kakao.com>
2021-12-29 17:22:01 +0900

    검색 결과 화면 레이아웃 구성 및 recyclerView 연동, cardView 속성 및 배경 색 바꿈

--- app/src/main/java/com/kunize/uswtimetable/ui/evaluation/EvaluationFragment.kt
+++ app/src/main/java/com/kunize/uswtimetable/ui/evaluation/EvaluationFragment.kt
@@ -27,18 +27,20 @@ class EvaluationFragment : Fragment() {
 
     private lateinit var evaluationViewModel : EvaluationViewModel
 
-    private val dummyData = arrayListOf(
-        EvaluationData("올뎃베이직12345678","교양대학",3.2f,3f,2.6f,4.2f,"기교"),
-        EvaluationData("디지털 사회의 마케팅(비대면 수업)","남아영",3.4f,4f,2.3f,3.9f,"기교"),
-        EvaluationData("데이터구조","이명원",0.4f,0.1f,0.2f,0.5f,"전핵"),
-        EvaluationData("컴퓨터구조","장성태",5f,0f,0f,0f,"전핵"),
-        EvaluationData("더미데이터","교양대학",4.9f,0f,0f,0f,"기교"),
-        EvaluationData("더미데이터","교양대학",3.2f,0f,0f,0f,"기교"),
-        EvaluationData("더미데이터","교양대학",3.2f,0f,0f,0f,"기교"),
-        EvaluationData("올뎃베이직","교양대학",3.2f,0f,0f,0f,"기교"),
-        EvaluationData("올뎃베이직","교양대학",3.2f,0f,0f,0f,"기교"),
-        EvaluationData("올뎃베이직","교양대학",3.2f,0f,0f,0f,"기교")
-    )
+    companion object {
+        val dummyData = arrayListOf(
+            EvaluationData("올뎃베이직12345678", "교양대학", 3.2f, 3f, 2.6f, 4.2f, "기교"),
+            EvaluationData("디지털 사회의 마케팅(비대면 수업)", "남아영", 3.4f, 4f, 2.3f, 3.9f, "기교"),
+            EvaluationData("데이터구조", "이명원", 0.4f, 0.1f, 0.2f, 0.5f, "전핵"),
+            EvaluationData("컴퓨터구조", "장성태", 5f, 0f, 0f, 0f, "전핵"),
+            EvaluationData("더미데이터", "교양대학", 4.9f, 0f, 0f, 0f, "기교"),
+            EvaluationData("더미데이터", "교양대학", 3.2f, 0f, 0f, 0f, "기교"),
+            EvaluationData("더미데이터", "교양대학", 3.2f, 0f, 0f, 0f, "기교"),
+            EvaluationData("올뎃베이직", "교양대학", 3.2f, 0f, 0f, 0f, "기교"),
+            EvaluationData("올뎃베이직", "교양대학", 3.2f, 0f, 0f, 0f, "기교"),
+            EvaluationData("올뎃베이직", "교양대학", 3.2f, 0f, 0f, 0f, "기교")
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,18 +56,25 @@ class EvaluationFragment : Fragment() {
         val spinnerImageList = listOf(R.drawable.ic_fire_24, R.drawable.ic_thumb_up_24, R.drawable.ic_star_24,
         R.drawable.ic_book_24, R.drawable.ic_best_24)
 
-        binding.searchLecture.setOnEditorActionListener { textView, it, keyEvent ->
+        //TODO 더보기 버튼 클릭 시 "정렬할 기준", "tags:ALL" 데이터를 가지고 프래그먼트 이동
+
+        binding.searchBtn.setOnClickListener {
+            goToSearchResult()
+        }
+
+        //키보드 검색 클릭 시 프래그먼트 이동 이벤트 구현
+        binding.searchLecture.setOnEditorActionListener { _, it, _ ->
             var handled = false
             if(it == EditorInfo.IME_ACTION_SEARCH) {
-                val action = EvaluationFragmentDirections.actionNavigationEvaluationToSearchResultFragment(textView.text.toString())
                 val inputMethodManager = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 inputMethodManager.hideSoftInputFromWindow(binding.searchLecture.windowToken, 0)
-                findNavController().navigate(action)
+                goToSearchResult()
                 handled = true
             }
             handled
         }
 
+        //spinner 설정
         val customSpinnerAdapter = CustomSpinnerAdapter(requireContext(), spinnerTextList, spinnerImageList)
         binding.spinner.apply {
             adapter = customSpinnerAdapter
@@ -91,5 +100,11 @@ class EvaluationFragment : Fragment() {
             }
         }
         return binding.root
+    }
+
+    private fun goToSearchResult() {
+        val action =
+            EvaluationFragmentDirections.actionNavigationEvaluationToSearchResultFragment(binding.searchLecture.text.toString())
+        findNavController().navigate(action)
     }
 }
--- app/src/main/java/com/kunize/uswtimetable/ui/search_result/SearchResultFragment.kt
+++ app/src/main/java/com/kunize/uswtimetable/ui/search_result/SearchResultFragment.kt
@@ -8,12 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.kunize.uswtimetable.R
-import com.kunize.uswtimetable.databinding.SearchResultFragmentBinding
+import com.kunize.uswtimetable.databinding.FragmentSearchResultBinding
+import com.kunize.uswtimetable.ui.evaluation.EvaluationFragment.Companion.dummyData
 import com.kunize.uswtimetable.ui.evaluation.EvaluationViewModel
 
 class SearchResultFragment : Fragment() {
 
-    lateinit var binding: SearchResultFragmentBinding
+    lateinit var binding: FragmentSearchResultBinding
     //EvaluationFragment와 동일한 viewModel 사용
     private lateinit var viewModel : EvaluationViewModel
 
@@ -21,14 +22,20 @@ class SearchResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = SearchResultFragmentBinding.inflate(inflater, container, false)
+        binding = FragmentSearchResultBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this)[EvaluationViewModel::class.java]
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
         val args: SearchResultFragmentArgs by navArgs()
         val msg = args.searchLectureName
-        binding.testText.text = msg
+
+        viewModel.changeData(ArrayList(dummyData.subList(0, 1)))
+
+        binding.searchLecture.apply {
+            setText(msg)
+            setSelection(msg.length)
+        }
         return binding.root
     }
 
