ff558f838bab0e3faea7d90045945a9cc26469de
mangbaam <pmb0836@gmail.com>
2021-12-30 23:57:47 +0900

    아이디, 비밀번호 찾기 뷰 작성
    아이디/ 비밀번호 찾기 화면에서 뒤로가기 버튼을 누르면 백스택을 정리하고 항상 로그인 화면으로 넘어감

---
+++ app/src/main/java/com/kunize/uswtimetable/login/FindPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.kunize.uswtimetable.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_password)
+    }
+}
