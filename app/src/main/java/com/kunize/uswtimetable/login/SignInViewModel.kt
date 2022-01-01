0e3524c025c4bb4f80d8eabcb4c91ad2484fe97f
mangbaam <pmb0836@gmail.com>
2022-01-02 00:37:58 +0900

    비밀번호 찾기, 아이디 찾기 -> 메테리얼 디자인 적용, 회원가입 UX 강화

---
+++ app/src/main/java/com/kunize/uswtimetable/login/SignInViewModel.kt
@@ -0,0 +1,22 @@
+package com.kunize.uswtimetable.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel: ViewModel() {
+    private var _isPwSame = MutableLiveData<PasswordAgain>()
+    val isPwSame: LiveData<PasswordAgain> get() = _isPwSame
+
+    val schoolMailUrl = "http://mail.suwon.ac.kr"
+
+    fun setPwSame() {
+        _isPwSame.value = PasswordAgain.SAME
+    }
+    fun setPwDifferent() {
+        _isPwSame.value = PasswordAgain.DIFFERENT
+    }
+    fun setPwEmpty() {
+        _isPwSame.value = PasswordAgain.EMPTY
+    }
+}
