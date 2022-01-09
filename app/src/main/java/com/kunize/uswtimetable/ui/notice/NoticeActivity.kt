480e782913b8e8654736b7a241e6ca933ed12e28
mangbaam <pmb0836@gmail.com>
2022-01-09 14:42:47 +0900

    스크롤 시에만 AppBar 그림자 적용
    공지사항 Toolbar 뒤로 가기 버튼 활성화

--- app/src/main/java/com/kunize/uswtimetable/ui/notice/NoticeActivity.kt
+++ app/src/main/java/com/kunize/uswtimetable/ui/notice/NoticeActivity.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,9 +12,7 @@ import com.kunize.uswtimetable.R
 import com.kunize.uswtimetable.adapter.NoticeAdapter
 import com.kunize.uswtimetable.databinding.ActivityNoticeBinding
 import com.kunize.uswtimetable.dataclass.NoticeData
-import com.kunize.uswtimetable.retrofit.RetrofitManager
 import com.kunize.uswtimetable.util.Constants.TAG
-import com.kunize.uswtimetable.util.ResponseState
 
 class NoticeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNoticeBinding
@@ -27,6 +23,10 @@ class NoticeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_notice)
+
+        binding.toolBar.setNavigationOnClickListener {
+            finish()
+        }
 
         viewModel = ViewModelProvider(this)[NoticeViewModel::class.java]
         // TODO 실제에는 dumpData가 아닌 noticeList로 대체
@@ -46,7 +46,8 @@ class NoticeActivity : AppCompatActivity() {
 
         val decorator = DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
         binding.noticeRecyclerView.adapter = adapter
-        binding.noticeRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true)
+        binding.noticeRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true)
         binding.noticeRecyclerView.addItemDecoration(decorator)
     }
 }
