868b55bc84c916db08d37977163077d34c178cee
mangbaam <pmb0836@gmail.com>
2022-03-24 06:08:36 +0900

    [REMOVE] Result.kt 와 같은 동작 하는 Output.kt 제거

--- app/src/main/java/com/kunize/uswtimetable/ui/repository/common/Output.kt
+++
@@ -1,6 +0,0 @@
-package com.kunize.uswtimetable.ui.repository.common
-
-sealed class Output<out T : Any> {
-    data class Success<out T: Any>(val output: T): Output<T>()
-    data class Error(val exception: Exception): Output<Nothing>()
-}
