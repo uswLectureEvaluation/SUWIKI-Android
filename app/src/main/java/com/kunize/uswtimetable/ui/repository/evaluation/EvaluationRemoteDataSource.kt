b2173315197fd70e1d2b6a3e5709fff1f5e9e96a
mangbaam <pmb0836@gmail.com>
2022-03-28 06:10:14 +0900

    [REMOVE] 동일한 동작하는 파일 제거

--- app/src/main/java/com/kunize/uswtimetable/ui/repository/evaluation/EvaluationRemoteDataSource.kt
+++ app/src/main/java/com/kunize/uswtimetable/ui/repository/evaluation/EvaluationRemoteDataSource.kt
@@ -1,21 +1,12 @@
 package com.kunize.uswtimetable.ui.repository.evaluation
 
-import android.util.Log
-import com.kunize.uswtimetable.dataclass.EvaluationData
 import com.kunize.uswtimetable.dataclass.LectureMainDto
-import com.kunize.uswtimetable.retrofit.ApiClient
 import com.kunize.uswtimetable.retrofit.IRetrofit
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.withContext
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
 
-class EvaluationRemoteDataSource(): EvaluationDataSource {
-    private val retrofit: IRetrofit by lazy { ApiClient.getClientWithNoToken().create(IRetrofit::class.java) }
+class EvaluationRemoteDataSource: EvaluationDataSource {
 
     override suspend fun getEvaluationDataSource(option: String, page: Int): Response<LectureMainDto> {
-        return retrofit.getLectureMainList(option)
+        return IRetrofit.getInstanceWithNoToken().getLectureMainList(option)
     }
 }
--- app/src/main/java/com/kunize/uswtimetable/ui/repository/search_result/SearchResultRemoteDataSource.kt
+++ app/src/main/java/com/kunize/uswtimetable/ui/repository/search_result/SearchResultRemoteDataSource.kt
@@ -1,25 +1,24 @@
 package com.kunize.uswtimetable.ui.repository.search_result
 
 import com.kunize.uswtimetable.dataclass.LectureMainDto
-import com.kunize.uswtimetable.retrofit.ApiClient
 import com.kunize.uswtimetable.retrofit.IRetrofit
 import com.kunize.uswtimetable.ui.repository.evaluation.EvaluationDataSource
 import retrofit2.Response
 
-class SearchResultRemoteDataSource() : SearchResultDataSource, EvaluationDataSource {
-    private val retrofit: IRetrofit by lazy { ApiClient.getClientWithNoToken().create(IRetrofit::class.java) }
+class SearchResultRemoteDataSource : SearchResultDataSource, EvaluationDataSource {
+    private val apiService = IRetrofit.getInstanceWithNoToken()
     override suspend fun getSearchResultDataSource(
         name: String,
         option: String,
         page: Int
     ): Response<LectureMainDto> {
-        return retrofit.getSearchResultDetail(name, option, page)
+        return apiService.getSearchResultDetail(name, option, page)
     }
 
     override suspend fun getEvaluationDataSource(
         option: String,
         page: Int
     ): Response<LectureMainDto> {
-        return retrofit.getLectureMainList(option, page)
+        return apiService.getLectureMainList(option, page)
     }
 }
