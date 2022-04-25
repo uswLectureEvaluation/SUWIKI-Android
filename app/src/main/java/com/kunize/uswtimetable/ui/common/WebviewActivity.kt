ef64cde783625df4e48e0913d2c33eebd4af5b6b
mangbaam <pmb0836@gmail.com>
2022-04-25 23:03:34 +0900

    [ADD] 웹뷰 추가

---
+++ app/src/main/java/com/kunize/uswtimetable/ui/common/WebviewActivity.kt
@@ -0,0 +1,31 @@
+package com.kunize.uswtimetable.ui.common
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.kunize.uswtimetable.R
+
+class WebviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+
+        val startUrl = intent.getStringExtra("url") ?: getString(R.string.SUWIKI_WEB)
+
+        val webView: WebView = findViewById(R.id.webView)
+        webView.apply {
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+            settings.supportZoom()
+            settings.builtInZoomControls = true
+            settings.javaScriptEnabled = true
+        }
+        webView.loadUrl(startUrl)
+
+        val closeButton: ImageView = findViewById(R.id.btn_close_webview)
+        closeButton.setOnClickListener { finish() }
+    }
+}
