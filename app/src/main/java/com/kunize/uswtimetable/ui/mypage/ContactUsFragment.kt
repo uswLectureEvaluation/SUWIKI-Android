f664e6b7ac3153547961c230a2cdfa17a058c2b0
mangbaam <pmb0836@gmail.com>
2022-05-09 22:55:19 +0900

    [ADD] 피드백 전송 - 이메일 연결 (임시)

--- app/src/main/java/com/kunize/uswtimetable/ui/mypage/ContactUsFragment.kt
+++ app/src/main/java/com/kunize/uswtimetable/ui/mypage/ContactUsFragment.kt
@@ -1,14 +1,18 @@
 package com.kunize.uswtimetable.ui.mypage
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.kunize.uswtimetable.R
 import com.kunize.uswtimetable.databinding.FragmentContactUsBinding
+import com.kunize.uswtimetable.util.afterTextChanged
 
 class ContactUsFragment: BottomSheetDialogFragment() {
 
@@ -32,15 +36,49 @@ class ContactUsFragment: BottomSheetDialogFragment() {
             category.adapter = adapter
         }
 
+        initViews()
+
 
         return binding.root
+    }
+
+    private fun initViews() {
+        with(binding) {
+            etTitle.afterTextChanged {
+                btnSend.isEnabled = onDataChanged()
+            }
+            etContent.afterTextChanged {
+                btnSend.isEnabled = onDataChanged()
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnSend.setOnClickListener {
-            // TODO 문의 내용 보내기
+            sendEmail()
+        }
+    }
+
+    private fun onDataChanged(): Boolean {
+        with(binding) {
+            return (etTitle.text.isNotBlank() && etContent.text.isNotBlank())
+        }
+    }
+
+    private fun sendEmail() {
+        val intent = Intent(Intent.ACTION_SENDTO)
+//        intent.type = "message/rfc822"
+//        intent.putExtra(Intent.EXTRA_EMAIL, "uswin_official@naver.com")
+        intent.data = Uri.parse("mailto:uswin_official@naver.com")
+        intent.putExtra(Intent.EXTRA_SUBJECT, binding.etTitle.text)
+        intent.putExtra(Intent.EXTRA_TEXT, binding.etContent.text)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        try {
+            startActivity(Intent.createChooser(intent, "문의하기"))
+        } catch(except: android.content.ActivityNotFoundException) {
+            Toast.makeText(requireContext(), "이메일을 보낼 수 없습니다", Toast.LENGTH_SHORT).show()
         }
     }
 
