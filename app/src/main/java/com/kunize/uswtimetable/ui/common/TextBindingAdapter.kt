e60c31070fc9b6da80e7e24be484b0d384b8e84e
mangbaam <pmb0836@gmail.com>
2022-05-17 07:11:21 +0900

    [ADD] user point binding adapter

--- app/src/main/java/com/kunize/uswtimetable/ui/common/TextBindingAdapter.kt
+++ app/src/main/java/com/kunize/uswtimetable/ui/common/TextBindingAdapter.kt
@@ -1,11 +1,24 @@
 package com.kunize.uswtimetable.ui.common
 
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.kunize.uswtimetable.R
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @BindingAdapter("dateTime")
 fun applyDateTime(view: TextView, dateTime: LocalDateTime) {
     view.text = dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+}
+
+@BindingAdapter("point")
+fun applyUserPoint(view: TextView, point: Int) {
+    if (point >= 0) {
+        view.text = "(+$point)"
+        view.setTextColor(ContextCompat.getColor(view.context, R.color.custom_red))
+    } else {
+        view.text = "${(point)}"
+        view.setTextColor(ContextCompat.getColor(view.context, R.color.custom_blue))
+    }
 }
