148119aa62363851e91acad795a34f4230634258
mangbaam <pmb0836@gmail.com>
2022-06-03 01:16:04 +0900

    [REFACTOR] delete LectureMainDto and wrap with DataDto

--- app/src/main/java/com/kunize/uswtimetable/data/remote/LectureMainDto.kt
+++ app/src/main/java/com/kunize/uswtimetable/data/remote/LectureMainDto.kt
@@ -1,36 +1,6 @@
 package com.kunize.uswtimetable.data.remote
 
 import com.google.gson.annotations.SerializedName
-import com.kunize.uswtimetable.data.local.EvaluationData
-import com.kunize.uswtimetable.util.LectureItemViewType.SHORT
-
-
-data class LectureMainDto(
-    val data: MutableList<LectureMain?>
-) {
-    fun convertToEvaluationData(): MutableList<EvaluationData?> {
-        val temp = mutableListOf<EvaluationData?>()
-        data.forEach {
-            it?.let {
-                temp.add(
-                    EvaluationData(
-                        recyclerViewType = SHORT,
-                        lectureId = it.id,
-                        yearSemester = it.semester,
-                        professor = it.professor,
-                        type = it.lectureType,
-                        name = it.lectureName,
-                        aver = it.lectureTotalAvg,
-                        satisfaction = it.lectureSatisfactionAvg,
-                        honey = it.lectureHoneyAvg,
-                        learning = it.lectureLearningAvg
-                    )
-                )
-            }
-        }
-        return temp
-    }
-}
 
 data class LectureMain(
     val id: Long,
--- app/src/main/java/com/kunize/uswtimetable/repository/search_result/SearchResultDataSource.kt
+++ app/src/main/java/com/kunize/uswtimetable/repository/search_result/SearchResultDataSource.kt
@@ -1,9 +1,9 @@
 package com.kunize.uswtimetable.repository.search_result
 
-import com.kunize.uswtimetable.data.remote.LectureMainDto
-import com.kunize.uswtimetable.data.remote.MajorType
+import com.kunize.uswtimetable.data.remote.DataDto
+import com.kunize.uswtimetable.data.remote.LectureMain
 import retrofit2.Response
 
 interface SearchResultDataSource {
-    suspend fun getSearchResultDataSource(name: String, option: String, page: Int, majorType: String): Response<LectureMainDto>
+    suspend fun getSearchResultDataSource(name: String, option: String, page: Int, majorType: String): Response<DataDto<MutableList<LectureMain?>>>
 }
