b3e7886b5327db28c1568fbeb877400884e3adcb
mangbaam <pmb0836@gmail.com>
2022-06-02 16:08:23 +0900

    [DELETE] unnecessary functions

--- app/src/main/java/com/kunize/uswtimetable/repository/my_post/MyPostDataSource.kt
+++ app/src/main/java/com/kunize/uswtimetable/repository/my_post/MyPostDataSource.kt
@@ -1,13 +1,13 @@
 package com.kunize.uswtimetable.repository.my_post
 
-import com.kunize.uswtimetable.dataclass.*
+import com.kunize.uswtimetable.dataclass.MyEvaluationListDto
+import com.kunize.uswtimetable.dataclass.MyExamInfoListDto
+import com.kunize.uswtimetable.dataclass.PurchaseHistoryDto
 import retrofit2.Response
 
 interface MyPostDataSource {
     suspend fun getMyEvaluations(page: Int): Response<MyEvaluationListDto>
     suspend fun getMyExamInfos(page: Int): Response<MyExamInfoListDto>
-    suspend fun editMyEvaluation(id: Long, request: MyEvaluationEditDto)
-    suspend fun editMyExamInfo(id: Long, request: MyExamInfoEditDto)
     suspend fun deleteMyEvaluation(id: Long)
     suspend fun deleteMyExamInfo(id: Long)
     suspend fun getPurchaseHistory(): Response<PurchaseHistoryDto>
--- app/src/main/java/com/kunize/uswtimetable/repository/my_post/MyPostRemoteDataSource.kt
+++ app/src/main/java/com/kunize/uswtimetable/repository/my_post/MyPostRemoteDataSource.kt
@@ -1,7 +1,5 @@
 package com.kunize.uswtimetable.repository.my_post
 
-import com.kunize.uswtimetable.dataclass.MyEvaluationEditDto
-import com.kunize.uswtimetable.dataclass.MyExamInfoEditDto
 import com.kunize.uswtimetable.retrofit.IRetrofit
 
 class MyPostRemoteDataSource(private val apiService: IRetrofit): MyPostDataSource {
@@ -9,10 +7,6 @@ class MyPostRemoteDataSource(private val apiService: IRetrofit): MyPostDataSourc
     override suspend fun getMyEvaluations(page: Int) = apiService.getEvaluatePosts(page)
 
     override suspend fun getMyExamInfos(page: Int) = apiService.getExamPosts(page)
-
-    override suspend fun editMyEvaluation(id: Long, request: MyEvaluationEditDto) = apiService.updateEvaluatePost(id, request)
-
-    override suspend fun editMyExamInfo(id: Long, request: MyExamInfoEditDto) = apiService.updateExamPost(id, request)
 
     override suspend fun deleteMyEvaluation(id: Long) {
         apiService.deleteEvaluation(id)
