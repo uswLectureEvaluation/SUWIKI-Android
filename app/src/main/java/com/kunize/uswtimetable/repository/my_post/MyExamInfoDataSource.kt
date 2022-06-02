095194e2ebf09df8e7142507f177416a14494aed
mangbaam <pmb0836@gmail.com>
2022-06-02 16:08:05 +0900

    [DELETE] unnecessary files

--- app/src/main/java/com/kunize/uswtimetable/repository/my_post/MyExamInfoDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.kunize.uswtimetable.repository.my_post
-
-import com.kunize.uswtimetable.dataclass.MyExamInfoListDto
-import retrofit2.Response
-
-interface MyExamInfoDataSource {
-    suspend fun getMyExamInfoData(page: Int): Response<MyExamInfoListDto>
-}
