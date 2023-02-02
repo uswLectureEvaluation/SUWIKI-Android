e17f3c153d0a79692a57d75b30a61dbef1c654d4
koownij <koownij@kakao.com>
2023-02-02 13:25:54 +0900

    refactor: 시간표 추가 시 시간 및 장소 검증 로직 추가

---
+++ app/src/main/java/com/kunize/uswtimetable/ui/class_info/filter/timeLocation/TimeLocationValidationFilter.kt
@@ -0,0 +1,59 @@
+package com.kunize.uswtimetable.ui.class_info.filter.timeLocation
+
+import android.widget.TextView
+import androidx.core.view.isVisible
+import com.kunize.uswtimetable.data.local.TimeData
+import com.kunize.uswtimetable.util.extensions.textToIntOrNull
+import com.kunize.uswtimetable.util.interceptingFilter.Filter
+import com.kunize.uswtimetable.util.interceptingFilter.FilterRequest
+import com.kunize.uswtimetable.util.interceptingFilter.FilterState
+import com.kunize.uswtimetable.util.interceptingFilter.UnknownFilterFail
+
+class TimeLocationValidationFilter : Filter {
+
+    override fun execute(request: FilterRequest): FilterState {
+        return if (request is TimeLocationValidationFilterRequest) checkAllTimeIsValid(request)
+            else UnknownFilterFail
+    }
+
+    private fun checkAllTimeIsValid(
+        request: TimeLocationValidationFilterRequest
+    ): FilterState {
+        with(request) {
+            for (idx in dayList.indices) {
+                if (checkTimeIsValid(
+                        deleteIdx = deleteIdx,
+                        tempDeleteData = tempDeleteData,
+                        tvDay = dayList[idx],
+                        location = locationList[idx],
+                        start = startTimeList[idx].textToIntOrNull(),
+                        end = endTimeList[idx].textToIntOrNull()
+                    ).not()
+                ) return TimeLocationNotValidate
+            }
+
+            return FilterState.Validate
+        }
+    }
+
+    private fun checkTimeIsValid(
+        deleteIdx: Int,
+        tempDeleteData: TimeData,
+        tvDay: TextView,
+        location: TextView,
+        start: Int?,
+        end: Int?
+    ): Boolean {
+        if (tvDay.text == "없음") return true
+        if (location.isVisible &&
+            (location.text.toString().isBlank() || start == null || end == null
+                    || (start > end
+                    || (start !in 1..15)
+                    || (end !in 1..15)))
+        ) {
+            //TODO 유효하지 않을 때 실행하는 코드
+            return false
+        }
+        return true
+    }
+}
---
+++ app/src/main/java/com/kunize/uswtimetable/ui/class_info/filter/timeLocation/TimeLocationValidationFilterRequest.kt
@@ -0,0 +1,15 @@
+package com.kunize.uswtimetable.ui.class_info.filter.timeLocation
+
+import android.widget.EditText
+import android.widget.TextView
+import com.kunize.uswtimetable.data.local.TimeData
+import com.kunize.uswtimetable.util.interceptingFilter.FilterRequest
+
+data class TimeLocationValidationFilterRequest(
+    val deleteIdx: Int,
+    val tempDeleteData: TimeData,
+    val dayList: List<TextView>,
+    val locationList: List<EditText>,
+    val startTimeList: List<EditText>,
+    val endTimeList: List<EditText>
+): FilterRequest
