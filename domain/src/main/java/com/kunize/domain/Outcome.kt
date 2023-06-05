7ca2ce1a8cf632c5bc7e7ed0f5fbb26a2a8d4642
jinukeu <koownij@kakao.com>
2023-06-05 16:15:54 +0900

    rename: Outcome의 Error 네이밍 Failure로 변경

--- domain/src/main/java/com/kunize/domain/Outcome.kt
+++ domain/src/main/java/com/kunize/domain/Outcome.kt
@@ -3,5 +3,5 @@ package com.kunize.domain
 sealed class Outcome<T> {
     object Loading : Outcome<Nothing>()
     data class Success<T>(val data: T) : Outcome<T>()
-    data class Error<T>(val error: Throwable?) : Outcome<T>()
+    data class Failure<T>(val error: Throwable?) : Outcome<T>()
 }
