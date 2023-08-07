7742ca0b12567ffb64be64cd148b448492f9f5a0
jinukeu <koownij@kakao.com>
2023-08-07 21:38:25 +0900

    feat: di 모듈 생성 및 build.gradle 세팅

---
+++ di/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("suwiki.android.library")
+    id("suwiki.android.hilt")
+    id("org.jetbrains.kotlin.plugin.serialization")
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.suwiki.di"
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(project(":data"))
+    implementation(project(":remote"))
+    implementation(project(":local"))
+
+    implementation(libs.bundles.coroutine)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+
+    implementation(libs.androidx.dataStore.core)
+    implementation(libs.androidx.dataStore.preferences)
+
+    ksp(libs.encrypted.datastore.preference.ksp)
+    implementation(libs.encrypted.datastore.preference.ksp.annotations)
+    implementation(libs.encrypted.datastore.preference.security)
+
+    ksp(libs.room.compiler)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+
+    implementation(libs.timber)
+}
