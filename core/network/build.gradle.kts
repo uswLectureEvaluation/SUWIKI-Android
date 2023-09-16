48915f63d1e794aeb2d1e0c7fde79d0af7b359a9
jinukeu <koownij@kakao.com>
2023-09-16 17:10:05 +0900

    feat: core:nework 모듈 생성

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,30 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("suwiki.android.library")
+    id("suwiki.android.hilt")
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+android {
+    namespace = "com.suwiki.core.network"
+}
+
+dependencies {
+    implementation(project(":core:model"))
+
+    implementation(libs.bundles.coroutine)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+    implementation(libs.androidx.monitor)
+    implementation(libs.androidx.junit.ktx)
+    androidTestImplementation(libs.junit4)
+
+    val bom = libs.firebase.bom
+    add("implementation", platform(bom))
+    implementation(libs.bundles.firebase)
+
+    implementation(libs.timber)
+}
