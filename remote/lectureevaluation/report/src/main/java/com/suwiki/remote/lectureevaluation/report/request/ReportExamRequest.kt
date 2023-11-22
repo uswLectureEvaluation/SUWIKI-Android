ae703780067322d54d8f1f98cf5aec5f7febe5c9
jinukeu <koownij@kakao.com>
2023-11-22 14:50:16 +0900

    fix: 누락된 `@Serializable` 추가

--- remote/lectureevaluation/report/src/main/java/com/suwiki/remote/lectureevaluation/report/request/ReportExamRequest.kt
+++ remote/lectureevaluation/report/src/main/java/com/suwiki/remote/lectureevaluation/report/request/ReportExamRequest.kt
@@ -1,5 +1,8 @@
 package com.suwiki.remote.lectureevaluation.report.request
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ReportExamRequest(
   val evaluateIdx: Long,
   val content: String = "",
