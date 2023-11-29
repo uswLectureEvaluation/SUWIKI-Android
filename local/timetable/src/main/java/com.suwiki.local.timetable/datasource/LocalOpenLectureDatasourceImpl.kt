82bfca4457a00df081c8acf8f0b45c5f1a8f1cd0
jinukeu <koownij@kakao.com>
2023-11-29 17:52:41 +0900

    chore: OpenLectureVersion 관련 메서드 추가

--- local/timetable/src/main/java/com.suwiki.local.timetable/datasource/LocalOpenLectureDatasourceImpl.kt
+++ local/timetable/src/main/java/com.suwiki.local.timetable/datasource/LocalOpenLectureDatasourceImpl.kt
@@ -1,15 +1,38 @@
 package com.suwiki.local.timetable.datasource
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.longPreferencesKey
 import com.suwiki.core.database.database.OpenLectureDatabase
+import com.suwiki.core.database.di.NormalDataStore
 import com.suwiki.core.model.timetable.OpenLecture
 import com.suwiki.data.timetable.datasource.LocalOpenLectureDatasource
 import com.suwiki.local.timetable.converter.toEntity
 import com.suwiki.local.timetable.converter.toModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class LocalOpenLectureDatasourceImpl @Inject constructor(
+  @NormalDataStore private val dataStore: DataStore<Preferences>,
   private val openLectureDatabase: OpenLectureDatabase,
 ) : LocalOpenLectureDatasource {
+
+  companion object {
+    private val LOCAL_OPEN_LECTURE_VERSION = longPreferencesKey("[KEY] is local open lecture version")
+  }
+
+  private val data: Flow<Preferences>
+    get() = dataStore.data
+
+  override suspend fun getOpenLectureListVersion(version: Long) {
+    dataStore.edit { it[LOCAL_OPEN_LECTURE_VERSION] = version }
+  }
+
+  override suspend fun setOpenLectureListVersion(): Flow<Long> {
+    return data.map { it[LOCAL_OPEN_LECTURE_VERSION] ?: 0L }
+  }
 
   override suspend fun getOpenLectureList(): List<OpenLecture> {
     return openLectureDatabase.openLectureDao().getAll().map { it.toModel() }
