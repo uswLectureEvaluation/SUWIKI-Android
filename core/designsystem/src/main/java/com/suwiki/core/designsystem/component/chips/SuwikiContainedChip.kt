445dc2f67eb30015dc2a1638c8484c0801dc33f0
BEEEAM-J <cbj0545@naver.com>
2023-12-05 20:08:20 +0900

    refactor: Contained, Outlined Chip 내부 텍스트 배치 방법 수정

--- core/designsystem/src/main/java/com/suwiki/core/designsystem/component/chips/SuwikiContainedChip.kt
+++ core/designsystem/src/main/java/com/suwiki/core/designsystem/component/chips/SuwikiContainedChip.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -51,16 +52,16 @@ fun SuwikiContainedChip(
       .clickable(onClick = onClick)
       .height(26.dp)
   ) {
-    Column(
+    Box(
       modifier = Modifier
-        .padding(top = 4.dp, bottom = 4.dp, start = 6.dp, end = 6.dp)
+        .padding(vertical = 4.dp, horizontal = 6.dp)
         .height(18.dp),
-      verticalArrangement = Arrangement.Center,
     ) {
       Text(
         text = text,
         color = contentColor,
         fontSize = 12.sp,
+        modifier = Modifier.align(Alignment.Center)
       )
     }
   }
