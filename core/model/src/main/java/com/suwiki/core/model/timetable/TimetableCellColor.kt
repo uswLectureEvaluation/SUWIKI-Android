2c0775e52e5e71b49e0ba41f637959ce2704be84
jinukeu <koownij@kakao.com>
2023-12-06 21:27:25 +0900

    design: TimetableCellColor 추가

---
+++ core/model/src/main/java/com/suwiki/core/model/timetable/TimetableCellColor.kt
@@ -0,0 +1,24 @@
+package com.suwiki.core.model.timetable
+
+enum class TimetableCellColor {
+  BROWN,
+  LIGHT_BROWN,
+  ORANGE,
+  APRICOT, // 살구
+  PURPLE,
+  PURPLE_LIGHT,
+  RED_LIGHT,
+  PINK,
+  BROWN_DARK,
+  GREEN_DARK,
+  GREEN,
+  GREEN_LIGHT,
+  NAVY_DARK,
+  NAVY,
+  NAVY_LIGHT,
+  VIOLET,
+  GRAY_DARK,
+  GRAY,
+  SKY,
+  VIOLET_LIGHT,
+}
