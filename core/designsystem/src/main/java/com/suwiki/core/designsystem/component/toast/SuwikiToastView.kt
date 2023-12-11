52e2977ea786ec5dda21114576e2d3cd4ff8a12b
lsakee <93514333+lsakee@users.noreply.github.com>
2023-12-11 19:36:05 +0900

    feat : #47 Suwiki Toast View

---
+++ core/designsystem/src/main/java/com/suwiki/core/designsystem/component/toast/SuwikiToastView.kt
@@ -0,0 +1,68 @@
+package com.suwiki.core.designsystem.component.toast
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SuwikiToastView(
+  messageTxt: String,
+) {
+  Box(
+    modifier = Modifier
+      .fillMaxSize(),
+    contentAlignment = Alignment.Center,
+  ) {
+    Surface(
+      modifier = Modifier
+        .wrapContentSize(),
+      color = Color.Transparent,
+    ) {
+      // 아이콘이 추가 될수 있을거 같아 일단 Row로 해두었습니다.
+      Row(
+        modifier = Modifier
+          .background(
+            color = Color(0xFF959595),
+            shape = RoundedCornerShape(25.dp),
+          )
+          .wrapContentWidth()
+          .wrapContentHeight()
+          .padding(8.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+      ) {
+        Text(
+          text = messageTxt,
+          textAlign = TextAlign.Center,
+          color = Color(0XFFFFFFFF),
+          modifier = Modifier.padding(PaddingValues(16.dp, 10.dp)),
+        )
+      }
+    }
+  }
+}
+
+@Preview
+@Composable
+fun SetViewPreview() {
+  SuwikiToastView(
+    messageTxt = "text",
+  )
+}
