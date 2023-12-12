a3d08b533d803d51e93a9b0a696eda31e69353ff
BEEEAM-J <cbj0545@naver.com>
2023-12-12 18:26:35 +0900

    refactor: SuwikiFilterButton 불필요한 Box 및 속성 제거

--- core/designsystem/src/main/java/com/suwiki/core/designsystem/component/filter/SuwikiFilterButton.kt
+++ core/designsystem/src/main/java/com/suwiki/core/designsystem/component/filter/SuwikiFilterButton.kt
@@ -1,11 +1,12 @@
 package com.suwiki.core.designsystem.component.filter
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,36 +28,39 @@ fun SuwikiFilterButton(
   Box(
     modifier = Modifier
       .suwikiClickable(onClick = onClick)
-      .height(32.dp)
+      .wrapContentHeight()
       .padding(vertical = 4.dp, horizontal = 8.dp),
   ) {
     Row(
       modifier = Modifier
-        .height(24.dp),
+        .wrapContentHeight(),
     ) {
       Text(
         text = text,
         color = Color(0xFF346CFD),
         fontSize = 16.sp,
-        modifier = Modifier.fillMaxHeight(),
-      )
-      Box(
         modifier = Modifier
-          .size(24.dp),
-      ) {
-        Icon(
-          painter = painterResource(id = R.drawable.ic_filter_arrow_down),
-          contentDescription = "",
-          tint = Color(0xFF346CFD),
-          modifier = Modifier.align(Alignment.Center),
-        )
-      }
+          .wrapContentHeight(),
+      )
+      Icon(
+        painter = painterResource(id = R.drawable.ic_filter_arrow_down),
+        contentDescription = "",
+        tint = Color(0xFF346CFD),
+        modifier = Modifier
+          .size(24.dp)
+          .padding(vertical = 9.dp, horizontal = 7.dp),
+      )
     }
   }
 }
 
-@Preview
+@Preview(widthDp = 400, heightDp = 50)
 @Composable
 fun SuwikiFilterButtonPreview() {
-  SuwikiFilterButton(text = "학과필터")
+  Column(
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally,
+  ) {
+    SuwikiFilterButton(text = "정보통신공학과")
+  }
 }
