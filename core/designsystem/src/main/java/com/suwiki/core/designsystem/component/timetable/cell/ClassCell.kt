a3025ceac7e96c0f8b746687a118fe903397f773
jinukeu <koownij@kakao.com>
2023-12-13 20:41:50 +0900

    refactor/#35: TimetableCellColor.toHex() 메서드의Cyclomatic Complexity 21 -> 1

--- core/designsystem/src/main/java/com/suwiki/core/designsystem/component/timetable/cell/ClassCell.kt
+++ core/designsystem/src/main/java/com/suwiki/core/designsystem/component/timetable/cell/ClassCell.kt
@@ -31,28 +31,30 @@ enum class TimetableCellType {
   CLASSNAME_PROFESSOR_LOCATION,
 }
 
-private fun TimetableCellColor.toHex(): Long = when (this) {
-  TimetableCellColor.BROWN -> 0xFFC28F62
-  TimetableCellColor.LIGHT_BROWN -> 0xFFD4AC89
-  TimetableCellColor.ORANGE -> 0xFFFFC152
-  TimetableCellColor.APRICOT -> 0xFFFFC152
-  TimetableCellColor.PURPLE -> 0xFF7E3348
-  TimetableCellColor.PURPLE_LIGHT -> 0xFFC97189
-  TimetableCellColor.RED_LIGHT -> 0xFFFE8888
-  TimetableCellColor.PINK -> 0xFFFDA1E4
-  TimetableCellColor.BROWN_DARK -> 0xFF614730
-  TimetableCellColor.GREEN_DARK -> 0xFF96B277
-  TimetableCellColor.GREEN -> 0xFFA5DC81
-  TimetableCellColor.GREEN_LIGHT -> 0xFF99ECA5
-  TimetableCellColor.NAVY_DARK -> 0xFF435796
-  TimetableCellColor.NAVY -> 0xFF5772C1
-  TimetableCellColor.NAVY_LIGHT -> 0xFF899DFE
-  TimetableCellColor.VIOLET -> 0xFFCC9AF3
-  TimetableCellColor.GRAY_DARK -> 0xFF525252
-  TimetableCellColor.GRAY -> 0xFFC2C1BD
-  TimetableCellColor.SKY -> 0xFF89C8FE
-  TimetableCellColor.VIOLET_LIGHT -> 0xFFC0C6E0
-}
+private val colorHexMap = mapOf(
+  TimetableCellColor.BROWN to 0xFFC28F62,
+  TimetableCellColor.LIGHT_BROWN to 0xFFD4AC89,
+  TimetableCellColor.ORANGE to 0xFFFFC152,
+  TimetableCellColor.APRICOT to 0xFFFFC152,
+  TimetableCellColor.PURPLE to 0xFF7E3348,
+  TimetableCellColor.PURPLE_LIGHT to 0xFFC97189,
+  TimetableCellColor.RED_LIGHT to 0xFFFE8888,
+  TimetableCellColor.PINK to 0xFFFDA1E4,
+  TimetableCellColor.BROWN_DARK to 0xFF614730,
+  TimetableCellColor.GREEN_DARK to 0xFF96B277,
+  TimetableCellColor.GREEN to 0xFFA5DC81,
+  TimetableCellColor.GREEN_LIGHT to 0xFF99ECA5,
+  TimetableCellColor.NAVY_DARK to 0xFF435796,
+  TimetableCellColor.NAVY to 0xFF5772C1,
+  TimetableCellColor.NAVY_LIGHT to 0xFF899DFE,
+  TimetableCellColor.VIOLET to 0xFFCC9AF3,
+  TimetableCellColor.GRAY_DARK to 0xFF525252,
+  TimetableCellColor.GRAY to 0xFFC2C1BD,
+  TimetableCellColor.SKY to 0xFF89C8FE,
+  TimetableCellColor.VIOLET_LIGHT to 0xFFC0C6E0,
+)
+
+private fun TimetableCellColor.toHex(): Long = colorHexMap[this]!!
 
 @Composable
 internal fun ClassCell(
