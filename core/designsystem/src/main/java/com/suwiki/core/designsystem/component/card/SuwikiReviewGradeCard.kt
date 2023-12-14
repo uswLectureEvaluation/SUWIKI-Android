5c7ecf113c3f2ef443f32d7eaabc5ec27f8c2ed2
lsakee <93514333+lsakee@users.noreply.github.com>
2023-12-15 02:21:38 +0900

    chore : #53 Preview

--- core/designsystem/src/main/java/com/suwiki/core/designsystem/component/card/SuwikiReviewGradeCard.kt
+++ core/designsystem/src/main/java/com/suwiki/core/designsystem/component/card/SuwikiReviewGradeCard.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -174,6 +175,7 @@ fun SuwikiReviewGradeCardPreview() {
         satisfactionCount = 4.4f,
         pressedBackgroundColor = Primary,
       )
+      Spacer(modifier = Modifier.height(20.dp))
       SuwikiReviewGradeCard(
         modifier = Modifier,
         onClick = {},
