2190a9d97bac4450c9ac4cad7c67ea4b78ebdc0f
jinukeu <koownij@kakao.com>
2023-12-21 12:43:17 +0900

    feat/#65: suwiki snackbar 추가

--- feature/navigator/src/main/java/com/suwiki/feature/navigator/MainScreen.kt
+++ feature/navigator/src/main/java/com/suwiki/feature/navigator/MainScreen.kt
@@ -14,13 +14,19 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -28,6 +34,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
+import com.suwiki.core.designsystem.component.toast.SuwikiToast
 import com.suwiki.core.designsystem.shadow.bottomNavigationShadow
 import com.suwiki.core.designsystem.theme.GrayDA
 import com.suwiki.core.designsystem.theme.Primary
@@ -36,12 +43,39 @@ import com.suwiki.core.designsystem.theme.White
 import com.suwiki.feature.lectureevaluation.viewerreporter.navigation.lectureEvaluationNavGraph
 import com.suwiki.feature.myinfo.navigation.myInfoNavGraph
 import com.suwiki.feature.timetable.navigation.timetableNavGraph
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+
+private const val SHOW_TOAST_LENGTH = 2000L
+val mutex = Mutex()
 
 @Composable
 internal fun MainScreen(
   modifier: Modifier = Modifier,
   navigator: MainNavigator = rememberMainNavigator(),
 ) {
+  val coroutineScope = rememberCoroutineScope()
+
+  var toastMessage: String? by remember { mutableStateOf(null) }
+  var toastVisible by remember { mutableStateOf(false) }
+
+  val onShowToast: (message: String) -> Unit = { message ->
+    coroutineScope.launch {
+      mutex.lock()
+      toastMessage = message
+    }
+  }
+
+  LaunchedEffect(key1 = toastMessage) {
+    if (toastMessage == null) return@LaunchedEffect
+
+    toastVisible = true
+    delay(SHOW_TOAST_LENGTH)
+    toastVisible = false
+    if (mutex.isLocked) mutex.unlock()
+  }
+
   Scaffold(
     modifier = modifier,
     content = { innerPadding ->
@@ -61,6 +95,11 @@ internal fun MainScreen(
           padding = innerPadding,
         )
       }
+
+      SuwikiToast(
+        visible = toastVisible,
+        message = toastMessage ?: "",
+      )
     },
     bottomBar = {
       MainBottomBar(
