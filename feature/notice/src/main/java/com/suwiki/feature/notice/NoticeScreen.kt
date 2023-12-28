a76a209d4685a19e8f1422bb008344fb59d951c2
BEEEAM-J <cbj0545@naver.com>
2023-12-29 04:31:20 +0900

    refactor/#73 : NoticeScreen

--- feature/notice/src/main/java/com/suwiki/feature/notice/NoticeScreen.kt
+++ feature/notice/src/main/java/com/suwiki/feature/notice/NoticeScreen.kt
@@ -2,36 +2,24 @@ package com.suwiki.feature.notice
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.suwiki.core.designsystem.component.appbar.SuwikiAppBarWithTitle
 import com.suwiki.core.designsystem.component.container.SuwikiNoticeContainer
 import com.suwiki.core.designsystem.component.loading.LoadingScreen
-import com.suwiki.core.designsystem.theme.Gray95
 import com.suwiki.core.designsystem.theme.SuwikiTheme
 import com.suwiki.core.designsystem.theme.White
 import com.suwiki.core.model.notice.Notice
-import com.suwiki.core.ui.extension.suwikiClickable
 import org.orbitmvi.orbit.compose.collectAsState
 import org.orbitmvi.orbit.compose.collectSideEffect
 import java.time.LocalDateTime
@@ -56,7 +44,7 @@ fun NoticeRoute(
     noticeList = viewModel.noticeList,
     uiState = uiState,
     navigateNoticeDetail = { viewModel.navigateNoticeDetail() },
-    popBackStack = { viewModel.popBackStack() },
+    popBackStack = viewModel::popBackStack,
   )
 }
 
@@ -69,65 +57,30 @@ fun NoticeScreen(
   navigateNoticeDetail: () -> Unit,
   popBackStack: () -> Unit,
 ) {
-  Scaffold(
-    modifier = Modifier.padding(padding),
-    topBar = {
-      NoticeScreenAppBar(
-        title = stringResource(R.string.notice),
-        onClickBack = popBackStack,
-      )
-    },
-    content = { appBarPadding ->
-      LazyColumn(
-        modifier = Modifier
-          .fillMaxSize()
-          .padding(appBarPadding)
-          .background(White),
-      ) {
-        items(items = noticeList) { notice ->
-          SuwikiNoticeContainer(
-            titleText = notice.title,
-            dateText = notice.date.toString(),
-            onClick = navigateNoticeDetail,
-          )
-        }
-      }
-    },
-  )
-
-  if (uiState.isLoading) {
-    LoadingScreen()
-  }
-}
-
-@Composable
-private fun NoticeScreenAppBar(
-  title: String = "",
-  onClickBack: () -> Unit = {},
-) {
-  Row(
+  Column(
     modifier = Modifier
-      .fillMaxWidth()
-      .wrapContentHeight()
-      .background(White)
-      .padding(top = 15.dp, bottom = 15.dp, start = 18.dp, end = 24.dp),
-    horizontalArrangement = Arrangement.SpaceBetween,
+      .padding(padding)
+      .fillMaxSize()
+      .background(White),
   ) {
-    Icon(
-      painter = painterResource(id = R.drawable.ic_appbar_arrow_left),
-      contentDescription = "",
-      tint = Gray95,
-      modifier = Modifier
-        .size(24.dp)
-        .clip(CircleShape)
-        .suwikiClickable(onClick = onClickBack)
-        .padding(vertical = 2.dp, horizontal = 6.5.dp),
+    SuwikiAppBarWithTitle(
+      title = stringResource(R.string.notice),
+      onClickBack = popBackStack,
+      enabledBack = true,
+      enabledRemove = false,
     )
-    Text(
-      text = title,
-      style = SuwikiTheme.typography.header6,
-    )
-    Spacer(modifier = Modifier.size(24.dp))
+    LazyColumn {
+      items(items = noticeList) { notice ->
+        SuwikiNoticeContainer(
+          titleText = notice.title,
+          dateText = notice.date.toString(),
+          onClick = navigateNoticeDetail,
+        )
+      }
+    }
+    if (uiState.isLoading) {
+      LoadingScreen()
+    }
   }
 }
 
