bb2efbb7c520c2a9e2358f01ed939ca9dc465d8c
lsakee <93514333+lsakee@users.noreply.github.com>
2023-12-31 20:00:33 +0900

    feat : #72 model

--- feature/lectureevaluation/viewerreporter/src/main/java/com/suwiki/feature/lectureevaluation/viewerreporter/model/LectureEvaluation.kt
+++ feature/lectureevaluation/viewerreporter/src/main/java/com/suwiki/feature/lectureevaluation/viewerreporter/model/LectureEvaluation.kt
@@ -1,11 +1,37 @@
 package com.suwiki.feature.lectureevaluation.viewerreporter.model
 
+import com.suwiki.core.model.lectureevaluation.lecture.LectureEvaluationAverage
+import kotlinx.collections.immutable.toPersistentList
+
 data class LectureEvaluation(
-  val id : Int,
-  val a: String,
-  val b: String,
-  val c: String,
-  val d: String,
-  val e: Float,
-  val f: Int,
+  val id: Long,
+  val lectureName: String,
+  val lectureType: String?,
+  val majorType: String,
+  val professor: String,
+  val lectureTotalAvg: Float,
+  val reviewCount: Int?,
 )
+
+fun List<LectureEvaluationAverage?>.toLectureEvaluation(
+  searchValue: String,
+) = filter {
+  if (searchValue.isNotEmpty()) {
+    searchValue in it!!.lectureInfo.lectureName
+    searchValue in it.lectureInfo.professor
+  }
+  else {
+    true
+  }
+}.map {
+  LectureEvaluation(
+    id = it!!.id,
+    lectureName = it.lectureInfo.lectureName,
+    lectureType = it.lectureInfo.lectureType,
+    majorType = it.lectureInfo.majorType,
+    professor = it.lectureInfo.professor,
+    lectureTotalAvg = it.lectureTotalAvg,
+    reviewCount = null
+  )
+}.toPersistentList()
+
