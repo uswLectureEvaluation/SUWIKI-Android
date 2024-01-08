e28c20333d72d982d9f1443d2741f3415b3946cf
jinukeu <koownij@kakao.com>
2024-01-08 22:15:30 +0900

    feat/#72: 기본 버퍼 3으로 설정

--- core/ui/src/main/java/com/suwiki/core/ui/extension/LazyListState.kt
+++ core/ui/src/main/java/com/suwiki/core/ui/extension/LazyListState.kt
@@ -12,7 +12,7 @@ import androidx.compose.runtime.snapshotFlow
 fun LazyListState.OnBottomReached(
   // tells how many items before we reach the bottom of the list
   // to call onLoadMore function
-  buffer: Int = 0,
+  buffer: Int = 3,
   onLoadMore: () -> Unit,
 ) {
   // Buffer must be positive.
