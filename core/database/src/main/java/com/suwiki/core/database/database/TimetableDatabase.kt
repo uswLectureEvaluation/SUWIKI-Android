b15f89d91d6dc8f425b514bfed6b9e959499c091
jinukeu <koownij@kakao.com>
2024-01-09 23:09:50 +0900

    chore/#85: schema 설정

--- core/database/src/main/java/com/suwiki/core/database/database/TimetableDatabase.kt
+++ core/database/src/main/java/com/suwiki/core/database/database/TimetableDatabase.kt
@@ -1,14 +1,25 @@
 package com.suwiki.core.database.database
 
+import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameColumn
+import androidx.room.RenameTable
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
 import com.suwiki.core.database.dao.TimeTableDao
+import com.suwiki.core.database.di.DatabaseName
 import com.suwiki.core.database.model.TimetableEntity
 
 @Database(
   entities = [TimetableEntity::class],
-  version = 1,
+  autoMigrations = [
+    AutoMigration(from = 1, to = 2, spec = TimetableDatabase.RenameTableAutoMigration::class),
+  ],
+  version = 2,
 )
 abstract class TimetableDatabase : RoomDatabase() {
   abstract fun timetableDao(): TimeTableDao
+
+  @RenameTable(fromTableName = "TimeTableList", toTableName = "TimetableEntity")
+  class RenameTableAutoMigration : AutoMigrationSpec
 }
