b270efd674736c51df84555f9255d6f21c46dffb
jinukeu <koownij@kakao.com>
2024-01-09 22:50:12 +0900

    chore: export schemas 설정

--- app/src/main/java/com/kunize/uswtimetable/data/local/TimeTableListDatabase.kt
+++ app/src/main/java/com/kunize/uswtimetable/data/local/TimeTableListDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [TimeTableList::class],version = 1)
+@Database(entities = [TimeTableList::class],version = 1, exportSchema = true)
 abstract class TimeTableListDatabase : RoomDatabase() {
     abstract fun timetableListDao(): TimeTableListDao
 
